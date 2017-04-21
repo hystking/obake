67e8894e47c2bdc0c7c54632deb2bef5a7382f19
Nakayama Yuhei <nakayama-yuhei@kayac.com>
2017-04-21 12:21:57 +0900

    remove scraper

--- index.js
+++ index.js
@@ -1,27 +1,10 @@
 const Koa = require("koa");
 const router = require("koa-router")();
-const scrape = require("./lib/scrape");
 
 const app = new Koa();
 
 router.get("/api/ping.json", async function (ctx) {
   ctx.body = {status: "alive"};
-});
-
-router.get("/api/scrape.json", async function (ctx) {
-  const url = ctx.request.query.url;
-  const res = {
-    url,
-    scraped_at: Date.now(),
-  };
-  try {
-    Object.assign(res, await scrape(url));
-  } catch(e) {
-    ctx.status = 500;
-    Object.assign(res, {error: e});
-  } finally {
-    ctx.body = res;
-  }
 });
 
 app
